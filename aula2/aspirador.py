c2b2899dc085ffa345be9f3fbb300a15b591be4f
Filipy <filipytav17@gmail.com>
2025-11-12 19:41:40 -0300

    Search almost complete

--- aula2/aspirador.py
+++ aula2/aspirador.py
@@ -5,6 +5,7 @@ def cleanTile(pos: list[int], env: list[list[int]]) -> None:
 def readSensor(pos: list[int], env: list[list[int]]) -> list[int]:
     y, x = pos
     print("position: (y, x)", y, x)
+    # The matrix is bi-dimensional and squared
     matrix_size: int = len(env[0])
     radius: int = 0
     found: bool = False
@@ -17,36 +18,22 @@ def readSensor(pos: list[int], env: list[list[int]]) -> list[int]:
         cols, rows = get_axes((y, x), radius, matrix_size)
         print(cols, rows)
 
-        # TODO: Eliminate redundancy: there's an overlap on the search of the rows and cols
+        # TODO: Eliminate redundancy: there's an overlap between the searchs of rows and cols
         for colx in cols:
-            if found:
+            row, col = search_matrix(env, colx, "col", y, radius)
+            if not (row < 0 or col < 0):
+                found = True
+                x = col
+                y = row
                 break
-
-            upper, lower = determine_bounds(y, radius, matrix_size)
-            for i in range(upper, lower):
-                col, row = colx, i
-                print("coords: ", row, col, ": ", env[row][col])
-
-                if env[row][col]:
-                    x = col
-                    y = row
-                    found = True
-                    break
 
         for rowy in rows:
-            if found:
+            row, col = search_matrix(env, rowy, "row", x, radius)
+            if not (row < 0 or col < 0):
+                found = True
+                x = col
+                y = row
                 break
-
-            upper, lower = determine_bounds(x, radius, matrix_size)
-            for i in range(upper, lower):
-                col, row = i, rowy
-                print("coords: ", row, col, ": ", env[row][col])
-
-                if env[row][col]:
-                    x = col
-                    y = row
-                    found = True
-                    break
 
             print("")
 
@@ -84,22 +71,48 @@ def get_axes(
     return (cols, rows)
 
 
-# (0, 1), (1, 0), (1, 1)
-environment: list[list[int]] = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-]
+# Return (y, x) if found, (-1, -1) otherwise
+def search_matrix(
+    matrix: list[list[int]],
+    axis: int,
+    axis_name: str,
+    axis_val: int,
+    radius: int,
+) -> tuple[int, int]:
+    matrix_size: int = len(matrix)
+    upper, lower = determine_bounds(axis_val, radius, matrix_size)
+    for i in range(upper, lower):
+        col = row = -1
+        if axis_name == "col":
+            col, row = axis, i
+        elif axis_name == "row":
+            col, row = i, axis
 
+        print("coords: ", row, col, ": ", matrix[row][col])
+
+        # x = col
+        # y = row
+        if matrix[row][col]:
+            return (row, col)
+    return (-1, -1)
+
+
+# (0, 1), (1, 0), (1, 1)
 # environment: list[list[int]] = [
-#     [9, 0, 0],
-#     [0, 0, 0],
-#     [0, 0, 0],
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 1],
 # ]
 
-pos: list[int] = [0, 2]
+environment: list[list[int]] = [
+    [0, 0, 0],
+    [0, 0, 1],
+    [9, 1, 0],
+]
+
+pos: list[int] = [2, 0]
 
 a = readSensor(pos, environment)
 print("sujo: ", a)
